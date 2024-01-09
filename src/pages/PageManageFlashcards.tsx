1b3cb59d8517232037e437dda5d7fe79e7896ce6
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 02:50:46 +0100

    add icon toggles

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../AppContext";
 import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
 import { SiOneplus } from "react-icons/si";
@@ -7,10 +7,11 @@ import { FaFloppyDisk } from "react-icons/fa6";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
+	const [isAdding, setIsAdding] = useState(true);
 	return (
 		<>
 			<p>There are {flashcards.length} flashcards:</p>
-			<table className="mt-4 dataTable">
+			<table className="mt-4 dataTable w-[80rem]">
 				<thead>
 					<tr>
 						<th>SUIID</th>
@@ -19,30 +20,32 @@ export const PageManageFlashcards = () => {
 						<th>Back</th>
 						<th>
 							<div className="flex justify-center">
-								<SiOneplus className="text-2xl cursor-pointer text-[#222] hover:text-[#1d411b]" />
+								<SiOneplus onClick={() => setIsAdding(!isAdding)} className="text-2xl cursor-pointer text-[#222] hover:text-[#1d411b]" />
 							</div>
 						</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td></td>
-						<td>
-							<input className="w-full" />
-						</td>
-						<td>
-							<input className="w-full" />
-						</td>
-						<td>
-							<input className="w-full" />
-						</td>
-						<td>
-							<div className="flex gap-2">
-								<FaFloppyDisk className="cursor-pointer text-[#222] hover:text-[#1d411b]" />
-								<MdCancel className="cursor-pointer text-[#222] hover:text-[#592727]" />
-							</div>
-						</td>
-					</tr>
+					{isAdding && (
+						<tr>
+							<td></td>
+							<td>
+								<input className="w-full" />
+							</td>
+							<td>
+								<input className="w-full" />
+							</td>
+							<td>
+								<input className="w-full" />
+							</td>
+							<td>
+								<div className="flex gap-2">
+									<FaFloppyDisk className="cursor-pointer text-[#222] hover:text-[#1d411b]" />
+									<MdCancel className="cursor-pointer text-[#222] hover:text-[#592727]" />
+								</div>
+							</td>
+						</tr>
+					)}
 					{flashcards.map((flashcard) => {
 						return (
 							<tr>
