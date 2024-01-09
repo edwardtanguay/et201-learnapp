7990f16a8ac57ae5a51ac306e8321500a76768ab
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 02:13:10 +0100

    icons in

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
+import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
+import { SiOneplus } from "react-icons/si";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
@@ -13,6 +15,11 @@ export const PageManageFlashcards = () => {
 						<th>Category</th>
 						<th>Front</th>
 						<th>Back</th>
+						<th>
+							<div className="flex justify-center">
+								<SiOneplus className="text-2xl cursor-pointer text-[#222] hover:text-[#000]" />
+							</div>
+						</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -23,6 +30,12 @@ export const PageManageFlashcards = () => {
 								<td>{flashcard.category}</td>
 								<td>{flashcard.front}</td>
 								<td>{flashcard.back}</td>
+								<td>
+									<div className="flex gap-2">
+										<FaPencilAlt className="cursor-pointer text-[#222] hover:text-[#000]" />{" "}
+										<FaRegTrashAlt className="cursor-pointer text-[#222] hover:text-[#000]" />
+									</div>
+								</td>
 							</tr>
 						);
 					})}
