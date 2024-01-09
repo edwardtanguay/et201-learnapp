ad26b4f373c59e65295c291d9aa0fe4b3a1026bd
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 02:31:07 +0100

    add row in

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { FaPencilAlt, FaRegTrashAlt } from "react-icons/fa";
 import { SiOneplus } from "react-icons/si";
+import { MdCancel } from "react-icons/md";
+import { FaFloppyDisk } from "react-icons/fa6";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
@@ -17,23 +19,41 @@ export const PageManageFlashcards = () => {
 						<th>Back</th>
 						<th>
 							<div className="flex justify-center">
-								<SiOneplus className="text-2xl cursor-pointer text-[#222] hover:text-[#000]" />
+								<SiOneplus className="text-2xl cursor-pointer text-[#222] hover:text-[#1d411b]" />
 							</div>
 						</th>
 					</tr>
 				</thead>
 				<tbody>
+					<tr>
+						<td></td>
+						<td>
+							<input className="w-full" />
+						</td>
+						<td>
+							<input className="w-full" />
+						</td>
+						<td>
+							<input className="w-full" />
+						</td>
+						<td>
+							<div className="flex gap-2">
+								<FaFloppyDisk className="cursor-pointer text-[#222] hover:text-[#1d411b]" />
+								<MdCancel className="cursor-pointer text-[#222] hover:text-[#592727]" />
+							</div>
+						</td>
+					</tr>
 					{flashcards.map((flashcard) => {
 						return (
 							<tr>
-								<td>{flashcard.suuid}</td>
+								<td className="font-mono">{flashcard.suuid}</td>
 								<td>{flashcard.category}</td>
 								<td>{flashcard.front}</td>
 								<td>{flashcard.back}</td>
 								<td>
 									<div className="flex gap-2">
-										<FaPencilAlt className="cursor-pointer text-[#222] hover:text-[#000]" />{" "}
-										<FaRegTrashAlt className="cursor-pointer text-[#222] hover:text-[#000]" />
+										<FaPencilAlt className="cursor-pointer text-[#222] hover:text-[#1d411b]" />
+										<FaRegTrashAlt className="cursor-pointer text-[#222] hover:text-[#592727]" />
 									</div>
 								</td>
 							</tr>
