fed01ae7711a1e6e87f6598e1510753ff26cb942
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-09 01:32:23 +0100

    styled table

--- src/pages/PageManageFlashcards.tsx
+++ src/pages/PageManageFlashcards.tsx
@@ -3,5 +3,31 @@ import { AppContext } from "../AppContext";
 
 export const PageManageFlashcards = () => {
 	const { flashcards } = useContext(AppContext);
-	return <p>There are {flashcards.length} flashcards:</p>;
+	return (
+		<>
+			<p>There are {flashcards.length} flashcards:</p>
+			<table className="mt-4 dataTable">
+				<thead>
+					<tr>
+						<th>SUIID</th>
+						<th>Category</th>
+						<th>Front</th>
+						<th>Back</th>
+					</tr>
+				</thead>
+				<tbody>
+					{flashcards.map((flashcard) => {
+						return (
+							<tr>
+								<td>{flashcard.suuid}</td>
+								<td>{flashcard.category}</td>
+								<td>{flashcard.front}</td>
+								<td>{flashcard.back}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</>
+	);
 };
